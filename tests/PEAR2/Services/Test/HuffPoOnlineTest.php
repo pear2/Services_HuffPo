ead116ccaa26b3b01f17a4c93595cb8f143145fd
till <till@php.net>
2012-05-03 17:46:55 +0200

    test multiple

--- tests/PEAR2/Services/Test/HuffPoOnlineTest.php
+++ tests/PEAR2/Services/Test/HuffPoOnlineTest.php
@@ -2,6 +2,7 @@
 namespace pear2\Services\Test;
 
 use pear2\Services\HuffPo;
+use pear2\Services\HuffPo\Request;
 
 class HuffPoOnlineTest extends \PHPUnit_Framework_TestCase
 {
@@ -18,5 +19,19 @@ class HuffPoOnlineTest extends \PHPUnit_Framework_TestCase
 
         $this->assertEquals(1441699, $meta->entry_id);
         $this->assertSame($url, $meta->entry_url);
+    }
+
+    public function testMultiple()
+    {
+        $request = new Request(
+            'http://www.huffingtonpost.com/api/?t=entry&entry_ids=1441699,1441698',
+            array(1441699,1441698)
+        );
+
+        $response = $request->makeRequest();
+        $object   = $request->parseResponse($response);
+
+        $this->assertObjectHasAttribute("1441699", $object);
+        $this->assertObjectHasAttribute("1441698", $object);
     }
 }
