3d24b5e624fea33da3e9ff79711c7f6c4f6a7813
till <till@php.net>
2012-05-03 16:38:05 +0200

    WIP test

--- tests/pear2/Services/Test/HuffPoOnlineTest.php
+++ tests/pear2/Services/Test/HuffPoOnlineTest.php
@@ -5,5 +5,15 @@ use pear2\Services\HuffPo;
 
 class HuffPoOnlineTest extends \PHPUnit_Framework_TestCase
 {
-
+    /**
+     * Simple integration test case until we mock the rest.
+     *
+     * @return void
+     */
+    public function testOnline()
+    {
+        $huffPo = new HuffPo('http://www.huffingtonpost.com/2012/04/20/vegan-is-love-book_n_1441699.html');
+        $meta   = $huffPo->getMetaData();
+        var_dump($meta);
+    }
 }
