0be9f9bcfce679097bc40b2d0de556bb958d0471
till <till@php.net>
2012-05-03 16:35:39 +0200

    include HTTP_Request2

--- tests/TestInit.php
+++ tests/TestInit.php
@@ -1,2 +1,8 @@
 <?php
+$status = (include_once 'HTTP/Request2.php');
+if (false === $status) {
+    echo "You need `pear install HTTP_Request2`." . PHP_EOL;
+    exit(1);
+}
+
 require_once dirname(__DIR__) . '/library/pear2/Services/HuffPo.php';
